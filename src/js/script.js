ba3efaab79955ef817fdf6a5f0a0ef294cb8feee
AhmedA <alkhatib_ahmed1995@hotmail.com>
2023-02-24 09:36:48 -0500

    added sugar button functionality, might change positioning of sugar later

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,16 @@
 // TODO:
 // SUGAR BUTTON
-// If sugar is clicked, add sugar sprinkling from top of screen downwards animation and sound effect.
+// If sugar is clicked, add sugar sprinkling from top of screen downwards animation. ✅
 // Bonus: if more than 3 sugar is added, send pop up saying "would you like some coffee with your sugar?"
 
 // CREAM BUTTON
-// If cream is clicked, show animation of cream pouring on screen and sound effect of liquid pouring.
+// If cream is clicked, show animation of cream splash on screen.
 // If user adds cream, coffee becomes lighter brown color, if no cream is added then coffee is dark brown.
 
 // BREW COFFEE BUTTON
-// When brew coffee button is clicked, all 3 buttons dissapear and the top section will display a reset button and "Brewing..." with bubbling sound effect
+// When brew coffee button is clicked, all 3 buttons dissapear and the top section will display a reset button and "Brewing..."
 // Follow by "Pouring..."
 // animation of coffee pouring into cup begins
-// sound effect of coffee pouring
 // Display now shows "Coffee is ready, enjoy! :)"
 // cup now is no longer empty and shows coffee inside (Color will depend on cream or no cream)
 // Bonus: add steam coming from top of cup effect?
@@ -20,6 +19,65 @@
 // interrupt the process and reset to start
 // reset to start whenever button is pressed, during or after process.
 
-const sugarBtn = document.querySelector(".sugar-btn");
 const creamBtn = document.querySelector(".cream-btn");
 const brewBtn = document.querySelector(".brew-btn");
+
+// ============================================ SUGAR BUTTON FUNCTIONALITY =================================================
+
+// Set the maximum duration and number of sugarGrains
+const maxDuration = 2000; // in milliseconds
+const maxSugarGrains = 100;
+
+// Create a timeline to manage the animation sequence
+const tl = gsap.timeline({ paused: true, onComplete: stopSugarFall });
+
+// Add sugarGrains to the timeline
+function addSugarGrains() {
+  for (let i = 0; i < maxSugarGrains; i++) {
+    let sugarGrain = document.createElement("div");
+    sugarGrain.className = "sugarGrain";
+    document.body.appendChild(sugarGrain);
+
+    // Randomize the starting position and speed of each sugarGrain
+    let startX = Math.random() * window.innerWidth;
+    let endX = startX + Math.random() * 100 - 50;
+    let endY = window.innerHeight + 10;
+    let duration = Math.random() * 3 + 1;
+
+    // Add the sugarGrain tween to the timeline
+    tl.to(sugarGrain, {
+      duration: duration,
+      x: endX,
+      y: endY,
+      ease: "power1.in",
+      repeat: -1,
+      delay: -duration,
+    }).fromTo(
+      sugarGrain,
+      { opacity: 0 },
+      { duration: 0.5, opacity: 0.8, ease: "power1.inOut" },
+      0
+    );
+  }
+
+  // Stop the animation after maxDuration
+  gsap.to(tl, { duration: maxDuration / 1000, onComplete: stopSugarFall });
+}
+
+// Stop the sugarfall animation when it's finished
+function stopSugarFall() {
+  tl.pause();
+  const sugarGrains = document.getElementsByClassName("sugarGrain");
+  while (sugarGrains[0]) {
+    sugarGrains[0].parentNode.removeChild(sugarGrains[0]);
+  }
+}
+
+// Event listener for button click
+const sugarBtn = document.querySelector(".sugar-btn");
+sugarBtn.addEventListener("click", function () {
+  addSugarGrains();
+  tl.play();
+});
+
+// ============================================ CREAM BUTTON FUNCTIONALITY =================================================
